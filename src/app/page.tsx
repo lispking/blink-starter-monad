8c2c0688c145382d8346034f152b50159d96e0b4
lispking <lispking@qq.com>
2025-05-13 10:10:08 +0800

    update blink url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
   // Action we want to execute in the Blink
   const { blink, isLoading } = useBlink({
-    url: "evm-action:http://localhost:3000/api/actions/donate-mon",
+    url: "evm-action:http://donate-mon.netlify.app/api/actions/donate-mon",
   });
 
   return (
